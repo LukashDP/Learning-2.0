d3acb1041674add84edc17a37d051abd5bef0427
Serhii <serhii@MacBookPro.home>
2025-08-06 22:12:48 +0200

    Change 5.3

--- LT3UA/Basic level python/Lesson_5/Lesson_5.3.py
+++ LT3UA/Basic level python/Lesson_5/Lesson_5.3.py
@@ -1,6 +1,4 @@
 list_words_str = input("Enter random words: ")
 list_words = list_words_str.split(" ")
 print(list_words)
-
-list_words = len(list_words)
-print("You entered", list_words, "words")
+print("You entered", len(list_words), "words")
