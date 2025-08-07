ca511bcbba519249ba91d7a13f800a1fbd98d3bd
Serhii <serhii@MacBookPro.home>
2025-08-07 20:53:45 +0200

    lesson_6

---
+++ LT3UA/Basic level python/Lesson_6/Lesson_6.2.py
@@ -0,0 +1,6 @@
+numbers = [-2, -1, 0, 1, 2]
+for i in numbers:
+    if i > 0:
+        print(i)
+    else:
+        print()
---
+++ LT3UA/Basic level python/Lesson_6/Lesson_6.3.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = []
+for i in numbers:
+    squared_numbers.append(i**2)
+print(squared_numbers)
+
