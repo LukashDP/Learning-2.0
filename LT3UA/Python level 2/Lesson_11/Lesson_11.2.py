007bb9a93170d6d59aaa50d000ee544ecfeecf72
Serhii <serhii@MacBookPro.home>
2025-08-15 14:23:49 +0200

    Lesson_11

---
+++ LT3UA/Python level 2/Lesson_11/Lesson_11.2.py
@@ -0,0 +1,4 @@
+# Створіть рядок зі слів "Python is awesome!" і виведіть тільки слово "awesome" у верхньому регістрі з використанням зрізу.
+
+list_2 = "Python is awesome!"
+print(list_2[10:-1].upper())
