57e3cc8a182b326b4285c01abca390df523c171a
Serhii <serhii@MacBookPro.home>
2025-08-25 22:12:24 +0200

    Lesson_15

---
+++ LT3UA/Python level 2/Lesson_15/Lesson_15.3.py
@@ -0,0 +1,7 @@
+# Напишіть програму для створення словника, де ключами будуть імена студентів, а значеннями - їхні середні бали
+
+ivanov = 3, 4, 5
+petrov = 6, 4, 3
+vasilev = 4, 9, 1
+dict_3 = ([["Ivanov", sum(ivanov)/len(ivanov)],["Petrov", sum(petrov)/len(petrov)], ["Vasilev", sum(vasilev)/len(vasilev)]])
+print(dict_3)
