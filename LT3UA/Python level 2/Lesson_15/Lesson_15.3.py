09d0af6a6eb3070af0ece226b60599b9a36af554
Serhii <serhii@MacBookPro.home>
2025-08-26 21:59:35 +0200

    Change in Lesson 15.3

--- LT3UA/Python level 2/Lesson_15/Lesson_15.3.py
+++ LT3UA/Python level 2/Lesson_15/Lesson_15.3.py
@@ -1,8 +1,17 @@
 # Напишіть програму для створення словника, де ключами будуть імена студентів, а значеннями - їхні середні бали
 
-ivanov = 3, 4, 5
-petrov = 6, 4, 3
-vasilev = 4, 9, 1
+# ivanov = 3, 4, 5
+# petrov = 6, 4, 3
+# vasilev = 4, 9, 1
+#
+# dict_3 = ([["Ivanov", round(sum(ivanov)/len(ivanov), 2)],["Petrov", round(sum(petrov)/len(petrov), 2)], ["Vasilev", round(sum(vasilev)/len(vasilev), 2)]])
+# print(dict_3)
 
-dict_3 = ([["Ivanov", round(sum(ivanov)/len(ivanov), 2)],["Petrov", round(sum(petrov)/len(petrov), 2)], ["Vasilev", round(sum(vasilev)/len(vasilev), 2)]])
-print(dict_3)
+
+students = {
+    "Ivanov": [3, 4, 5],
+    "Petrov": [6, 4, 3],
+    "Vasilev": [4, 9, 1]
+}
+average_points = {name: round(sum(grades) / len(grades), 2) for name, grades in students.items()}
+print(average_points)
