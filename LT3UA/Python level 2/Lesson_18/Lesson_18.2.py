7f61b52019998bc1e0fca198146c4fda877a7387
Serhii <serhii@MacBookPro.home>
2025-09-02 21:25:58 +0200

    Lesson_18

---
+++ LT3UA/Python level 2/Lesson_18/Lesson_18.2.py
@@ -0,0 +1,7 @@
+# Створіть функцію для обчислення суми двох чисел що запитує їх у користувача і друку результату
+def summa():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print(a + b)
+
+summa()
